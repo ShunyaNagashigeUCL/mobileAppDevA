4d111674ddbecbf61c8c717ae37f39cf7bc49699
ShunyaNagashige <shunya.nagashige@ucl.meijo-u.ac.jp>
2023-09-05 13:09:37 +0900

    演習8の動作のみ実現

--- app/src/main/java/jp/ac/meijou/android/s233426012/MainActivity.java
+++ app/src/main/java/jp/ac/meijou/android/s233426012/MainActivity.java
@@ -3,12 +3,52 @@ package jp.ac.meijou.android.s233426012;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.TextView;
+
+import jp.ac.meijou.android.s233426012.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+    private ActivityMainBinding binding;
+    private PrefDataStore dataStore;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        dataStore = PrefDataStore.getInstance(getApplicationContext());
+        dataStore.getString("name")
+                .ifPresent(name -> binding.text.setText(name));
+
+
+        binding.changeButton.setOnClickListener(view ->{
+            var text = binding.editTextText.getText().toString();
+            binding.text.setText(text);
+        });
+
+        binding.saveButton.setOnClickListener(view -> {
+            var text = binding.editTextText.getText().toString();
+            dataStore.setString("name", text);
+        });
+
+        binding.editTextText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                // テキストが更新される直前に呼ばれる
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                // 文字を1つ入力された時に呼ばれる
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                // テキストが更新されたあとに呼ばれる
+//                binding.text.setText(editable.toString());
+            }
+        });
     }
 }
